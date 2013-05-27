6168b123a39e3f8b7c677dbc47418c2f164daec3
sivareddykothuru <siva@agiliq.com>
2013-05-27 19:49:57 +0530

    program done using command line arguments

---
+++ ex13.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+script, first, second, third = argv
+forth = raw_input("Enter 4th variable: ")
+print "The script is called:", script
+print "Your first variable is:", first
+print "YOur second variable is:", second
+print "Your third variable is:", third
+print "Your 4th variable is:", forth

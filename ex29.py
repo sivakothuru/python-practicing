db1b52ac43020e484f7fd8a97a0f61464ae4e319
sivareddykothuru <siva@agiliq.com>
2013-05-28 15:54:05 +0530

    code explaining if condition

---
+++ ex29.py
@@ -0,0 +1,29 @@
+people = 50
+cats = 20
+dogs = 30
+
+if people < cats:
+    print "Too many cats. The world is doomed"
+
+if people > cats:
+    print "Not many cats. The world is saved"
+
+if people < dogs:
+    print "The world is drooled on!"
+
+if people > dogs:
+    print "The world is dry!"
+
+dogs +=5
+
+if people >= dogs:
+    print "People are greater than are equak to dogs"
+
+if people <= dogs:
+    print "People are less than or equal to dogs"
+
+if people == dogs:
+    print "People are dogs"
+
+if people != cats:
+    print "people and cats are different"

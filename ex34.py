97dba4d2018cc1fb032b41aff0edd0174908fffa
sivareddykothuru <siva@agiliq.com>
2013-05-28 18:35:37 +0530

    code showing the accesing of elements from the list

---
+++ ex34.py
@@ -0,0 +1,10 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print "1. The animal at 0 is the 1st animal and is a", animals[0]
+print "2. The 3rd animal is at 2 and is a", animals[2]
+print "3. The 1st animal is at 0 and is a", animals[0]
+print "4. The animal at 3 is the 4th animal and is a", animals[3]
+print "5. The 5th animal is at 4 and is a", animals[4]
+print "6. The animal at 2 is the 3rd animal and is a", animals[2]
+print "7. The 6th animal is at 5 and is a", animals[5]
+print "8. The animal at 4 is the 5th animal and is a", animals[4]
